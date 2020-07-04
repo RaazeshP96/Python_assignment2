55a4b939671e098f86bc9ccd713282de127c5fca
Raazeshp96 <raazeshp96@gmail.com>
2020-07-04 10:56:12 +0545

    qn3 solved

--- assignment03.py
+++ assignment03.py
@@ -3,6 +3,27 @@ Write code that will print out the anagrams (words that use the same
 letters) from a paragraph of text.
 
 '''
+from itertools import combinations
 
-import unittest
 
+def anagrams(sentence):
+    # spliting the sentence into words
+    wordList = sentence.split(' ')
+    # list of all the possible combination of 2 words
+    comb = list(combinations(wordList, 2))
+    s = 0
+    for i in comb:
+        str1 = list(i[0])
+        str1.sort()
+        str2 = list(i[1])
+        str2.sort()
+        if str1 == str2:
+            s = s + 1
+    return s
+
+
+sen = 'This is cat and dan si tac'
+sentence = sen.upper()
+
+angms = anagrams(sentence)
+print(f'There are {angms} anagrams')
